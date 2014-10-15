04dcaa7051bf81be96ac917b25b732c3b5fb1128
Cameron <cameron.p.buckingham@gmail.com>
2014-10-15 14:02:16 -0600

    working and tested

--- lib/menu_items.rb
+++ lib/menu_items.rb
@@ -1,65 +1,47 @@
-class MenuCalculator
-  def initialize
-  end
-
-  def read_file
-    arr = []
-    table = CSV.foreach('data/menu_data.csv', headers: true)
-    table.each do |row|
-      new = row.to_h
-      arr << new
-    end
-  end
-  end
 require 'csv'
 
-arr = []
-table = CSV.foreach('data/menu_data.csv', headers: true)
-table.each do |row|
-  new = row.to_h
-  arr << new
-end
-
-def longest_receipt(arr)
-  total = 15.05
-  lowest_price = arr.inject do |memo, hash|
-    memo["price"] < hash["price"] ? memo : hash
-  end
-  ((total / lowest_price["price"].to_f).to_i) - 1
-end
-
-def totaling_array(arr)
-  all_possible_receipts = []
-  totaling = longest_receipt(arr)
-  totaling.times do |i|
-    arr.repeated_combination(i+1).each do |combos|
-      all_possible_receipts << combos
+class MenuCalculator
+  def initialize(money, csv)
+    @money = money
+    @arr = []
+    table = CSV.foreach(csv, headers: true)
+    table.each do |row|
+      new = row.to_h
+      @arr << new
     end
   end
-  validator = valid_receipts_from_all(all_possible_receipts)
-  if validator != []
-    validator.map do |final_items|
-      p "Valid Combination:"
-      print "#{final_items}\n\n"
+
+  def longest_receipt
+    lowest_price = @arr.inject do |memo, hash|
+      memo["price"] < hash["price"] ? memo : hash
     end
-  else
-    p "Sorry no menu items match that price"
+    ((@money / lowest_price["price"].to_f).to_i) - 1
   end
-end
 
-def array_total(arr)
-  total = 0.0
-  arr.each do |hash|
-    total += hash["price"].to_f
+  def totaling_array
+    result = []
+    totaling = longest_receipt
+    totaling.times do |i|
+      @arr.repeated_combination(i+1).each do |combos|
+        if array_total(combos) == @money
+          result << combos
+        end
+      end
+    end
+    if result != []
+      result
+    else
+      nil
+    end
   end
-  total
+
+  def array_total(arr)
+    arr.inject(0) { |memo, hash| memo += hash["price"].to_f; memo }
+  end
+
 end
 
-def valid_receipts_from_all(arr)
-  arr.select { |receipt| array_total(receipt) == 15.05 }
-end
-
-totaling_array(arr)
-
+cal = MenuCalculator.new(90.05, 'data/menu_data.csv')
+p cal.totaling_array
 
 
--- spec/menu_items_spec.rb
+++ spec/menu_items_spec.rb
@@ -3,13 +3,25 @@ require 'menu_items'
 
 
 describe 'MenuCalculator' do
-  def parse_response
-    MenuCalculator.new.read_file
+  # def parse_response
+  #   MenuCalculator.new
+  # end
+
+
+  it 'should return various combinations' do
+    calc = MenuCalculator.new(15.05, 'data/menu_data.csv')
+    expect(calc.totaling_array).to eq([[{"food"=>"mixed fruit", "price"=>"2.15"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"sampler plate", "price"=>"5.80"}]])
   end
 
-describe 'calculate menu items with 15.05' do
-  it 'should return various combinations' do
-    expect(parse_response).to eq([{"food"=>"mixed fruit", "price"=>"2.15"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"sampler plate", "price"=>"5.80"}])
+  it 'should return nil when no combination is present' do
+    calc = MenuCalculator.new(1.05, 'data/menu_data.csv')
+    expect(calc.totaling_array).to eq(nil)
+  end
+
+  it 'should return various combinations for a different number' do
+    calc = MenuCalculator.new(10.05, 'data/menu_data.csv')
+    expect(calc.totaling_array).to eq([[{"food"=>"side salad", "price"=>"3.35"}, {"food"=>"side salad", "price"=>"3.35"}, {"food"=>"side salad", "price"=>"3.35"}]])
   end
 end
-end
+
+

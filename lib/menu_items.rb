dec72bf1e6551a8f5a42581dedabfe6f0f657f2b
Cameron <cameron.p.buckingham@gmail.com>
2014-10-16 12:02:42 -0600

    ready to go

--- lib/menu_items.rb
+++ lib/menu_items.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 class MenuCalculator
   def initialize(source_file)
     File.open(source_file, 'r') do |f|
@@ -7,48 +5,58 @@ class MenuCalculator
       f.each_line do |line|
         arr << line
       end
-      @money = arr[0][1..-1].to_f
-      @array = arr[1..-1]
+      @money = removes_money_symbol(arr[0]).to_f
+      @array = removes_money_symbol(arr)
     end
-    @array = @array.reduce(Array.new) do |memo, str|
-      hash = {}
-      arr = str.split(",")
-      hash[:food] = arr[0]
-      hash[:price] = arr[1][1..-1].to_f
-      memo << hash
-    end
+    @array = builds_hashed_array(@array)
   end
 
-  def longest_receipt
-    lowest_price = @array.inject do |memo, hash|
-      memo[:price] < hash[:price] ? memo : hash
-    end
-    ((@money / lowest_price[:price]).to_i)
+def longest_receipt
+  lowest_price = @array.inject do |memo, hash|
+    memo[:price] < hash[:price] ? memo : hash
   end
+  ((@money / lowest_price[:price]).to_i)
+end
 
-  def totaling_array
-    result = []
-    totaling = longest_receipt
-    totaling.times do |i|
-      @array.repeated_combination(i+1).each do |combos|
-        if array_total(combos) == @money
-          result << combos
-        end
+def totaling_array
+  result = []
+  totaling = longest_receipt
+  totaling.times do |i|
+    @array.repeated_combination(i+1).each do |combos|
+      if array_total(combos) == @money
+        result << combos
       end
     end
-    if result != []
-      result
-    else
-      nil
-    end
   end
-
-  def array_total(arr)
-    arr.inject(0) { |memo, hash| memo += hash[:price].to_f; memo }
+  if result != []
+    result
+  else
+    nil
   end
+end
 
+def array_total(arr)
+  arr.inject(0) { |memo, hash| memo += hash[:price].to_f; memo }
+end
+
+private
+
+def removes_money_symbol(arr)
+  arr[1..-1]
+end
+
+def builds_hashed_array(data)
+  data.reduce(Array.new) do |memo, str|
+    hash = {}
+    arr = str.split(",")
+    hash[:food] = arr[0]
+    hash[:price] = arr[1][1..-1].to_f
+    memo << hash
+  end
 end
 
 cal = MenuCalculator.new('data/menu_items.txt')
 p cal.totaling_array
+
+end
 

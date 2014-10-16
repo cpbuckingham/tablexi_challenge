8e290acc2e86e6b68837c9f48d9122a1c940810b
Cameron <cameron.p.buckingham@gmail.com>
2014-10-16 10:52:56 -0600

    reading from a txt file now

--- lib/menu_items.rb
+++ lib/menu_items.rb
@@ -1,28 +1,36 @@
 require 'csv'
 
 class MenuCalculator
-  def initialize(money, csv)
-    @money = money
-    @arr = []
-    table = CSV.foreach(csv, headers: true)
-    table.each do |row|
-      new = row.to_h
-      @arr << new
+  def initialize(source_file)
+    File.open(source_file, 'r') do |f|
+      arr = []
+      f.each_line do |line|
+        arr << line
+      end
+  @money = arr[0][1..-1].to_f
+    @array = arr[1..-1]
+    end
+  @array = @array.reduce(Array.new) do |memo, str|
+      hash = {}
+      arr = str.split(",")
+      hash[:food] = arr[0]
+      hash[:price] = arr[1][1..-1].to_f
+      memo << hash
     end
   end
 
   def longest_receipt
-    lowest_price = @arr.inject do |memo, hash|
-      memo["price"] < hash["price"] ? memo : hash
+    lowest_price = @array.inject do |memo, hash|
+      memo[:price] < hash[:price] ? memo : hash
     end
-    ((@money / lowest_price["price"].to_f).to_i) - 1
+    ((@money / lowest_price[:price]).to_i)
   end
 
   def totaling_array
     result = []
     totaling = longest_receipt
     totaling.times do |i|
-      @arr.repeated_combination(i+1).each do |combos|
+      @array.repeated_combination(i+1).each do |combos|
         if array_total(combos) == @money
           result << combos
         end
@@ -36,12 +44,11 @@ class MenuCalculator
   end
 
   def array_total(arr)
-    arr.inject(0) { |memo, hash| memo += hash["price"].to_f; memo }
+    arr.inject(0) { |memo, hash| memo += hash[:price].to_f; memo }
   end
 
 end
 
-cal = MenuCalculator.new(90.05, 'data/menu_data.csv')
+cal = MenuCalculator.new('data/menu_items.txt')
 p cal.totaling_array
-
 
--- spec/menu_items_spec.rb
+++ spec/menu_items_spec.rb
@@ -3,23 +3,19 @@ require 'menu_items'
 
 
 describe 'MenuCalculator' do
-  # def parse_response
-  #   MenuCalculator.new
-  # end
-
 
   it 'should return various combinations' do
-    calc = MenuCalculator.new(15.05, 'data/menu_data.csv')
+    calc = MenuCalculator.new('data/menu_data.csv')
     expect(calc.totaling_array).to eq([[{"food"=>"mixed fruit", "price"=>"2.15"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"hot wings", "price"=>"3.55"}, {"food"=>"sampler plate", "price"=>"5.80"}]])
   end
 
   it 'should return nil when no combination is present' do
-    calc = MenuCalculator.new(1.05, 'data/menu_data.csv')
+    calc = MenuCalculator.new('data/menu_data.csv')
     expect(calc.totaling_array).to eq(nil)
   end
 
   it 'should return various combinations for a different number' do
-    calc = MenuCalculator.new(10.05, 'data/menu_data.csv')
+    calc = MenuCalculator.new('data/menu_data.csv')
     expect(calc.totaling_array).to eq([[{"food"=>"side salad", "price"=>"3.35"}, {"food"=>"side salad", "price"=>"3.35"}, {"food"=>"side salad", "price"=>"3.35"}]])
   end
 end
